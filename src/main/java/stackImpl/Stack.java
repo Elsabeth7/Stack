66f824f08b845df6affe8e1c0bd915f12cfb549b
elsabeth <elsabeth24@gmail.com>
2020-01-27 23:39:10 -0600

    stack methods added

--- src/main/java/stackImpl/Stack.java
+++ src/main/java/stackImpl/Stack.java
@@ -1,10 +1,33 @@
 package stackImpl;
 
 public class Stack {
+	
+	private Object stack = new Object();
+	
+	public boolean isEmpty() {
+		if(stack == null) {
+			return false;
+		}		
+		return true;
+	}
 
-	public int size() {
+	public void push(Object input) {
 		
-		return 0;
+		stack = input;
+		
+	}
+
+	public Object pop() throws Exception {
+		if(stack == null) {
+			throw new Exception();
+		}
+		Object result = stack;
+		stack = null;		
+		return result;
+	}
+
+	public Object peek() {
+		return stack;
 	}
 
 }
--- src/test/java/stackImplTest/StackTest.java
+++ src/test/java/stackImplTest/StackTest.java
@@ -1,17 +1,39 @@
 package stackImplTest;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import stackImpl.Stack;
 
 public class StackTest {
+	Stack stack = new Stack();
+	
 	@Test
-	public void testStack_InitialSize() {
-		Stack stack = new Stack();
-		Assert.assertEquals(0, stack.size());
+	public void testStack_isEmpty() {
+		
+		assertEquals(true, stack.isEmpty());
 	}
 
+	@Test
+	public void testStack_Pop() throws Exception {
+		
+		stack.push(1);
+		Object result = stack.pop();
+		assertEquals(1, result);
+	}
 	
+	@Test
+	public void testStack_peek() {
+		stack.push(7);
+		Object result = stack.peek();
+		assertEquals(7, result);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testStackPop_EmptyWithValue() throws Exception {
+		stack.pop();
+	}
 	
 }
